9511dc044e59821188f5613355c08644d2c49c83
Paul Jones <p.jones@outlook.com>
2024-12-21 13:11:16 +0000

    avm 001

--- main.tf
+++ main.tf
@@ -1,30 +1,93 @@
-
-locals {
-  base_name = "${var.company}-${var.prefix}-${var.workload}"
+provider "azurerm" {
+  features {}
 }
 
-
-
-resource "azurerm_resource_group" "rg_uk001" {
-  name = "${local.base_name}-uk-rg"
-  location = var.locations.uk
+terraform {
+  required_version = "~> 1.9"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.71"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
 }
 
-resource "azurerm_resource_group" "rg_us001" {
-  name = "${local.base_name}-us-rg"
-  location = var.locations.us
+module "regions" {
+  source  = "Azure/avm-utl-regions/azurerm"
+  version = "0.1.0"
 }
 
-resource "azurerm_virtual_network" "vnet_uk001" {
-  name                = "${local.base_name}-uk-vnet"
-  location            = var.locations.uk
-  resource_group_name = azurerm_resource_group.rg_uk001.name
-  address_space       = var.vnet_address_space-uk
+# This allows us to randomize the region for the resource group.
+resource "random_integer" "region_index" {
+  max = length(module.regions.regions) - 1
+  min = 0
 }
 
-resource "azurerm_virtual_network" "vnet_uk001" {
-  name                = "${local.base_name}-us-vnet"
-  location            = var.locations.uks
-  resource_group_name = azurerm_resource_group.rg_us001.name
-  address_space       = var.vnet_address_space-us
+# This ensures you have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.3.0"
+}
+
+resource "azurerm_resource_group" "this" {
+  location = module.regions.regions[random_integer.region_index.result].name
+  name     = module.naming.resource_group.name_unique
+}
+
+# Get current IP address for use in KV firewall rules
+data "http" "ip" {
+  url = "https://api.ipify.org/"
+  retry {
+    attempts     = 5
+    max_delay_ms = 1000
+    min_delay_ms = 500
+  }
+}
+
+data "azurerm_client_config" "current" {}
+
+module "key_vault" {
+  source                        = "Azure/avm-res-keyvault-vault/azurerm"
+  name                          = module.naming.key_vault.name_unique
+  location                      = azurerm_resource_group.this.location
+  enable_telemetry              = var.enable_telemetry
+  resource_group_name           = azurerm_resource_group.this.name
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  public_network_access_enabled = true
+  keys = {
+    cmk_for_storage_account = {
+      key_opts = [
+        "decrypt",
+        "encrypt",
+        "sign",
+        "unwrapKey",
+        "verify",
+        "wrapKey"
+      ]
+      key_type = "RSA"
+      name     = "cmk-for-storage-account"
+      key_size = 2048
+    }
+  }
+  role_assignments = {
+    deployment_user_kv_admin = {
+      role_definition_id_or_name = "Key Vault Administrator"
+      principal_id               = data.azurerm_client_config.current.object_id
+    }
+  }
+  wait_for_rbac_before_key_operations = {
+    create = "60s"
+  }
+  network_acls = {
+    bypass   = "AzureServices"
+    ip_rules = ["${data.http.ip.response_body}/32"]
+  }
 }
