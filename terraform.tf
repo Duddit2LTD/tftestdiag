9ad9f0b9dea08c28bad50e93c2b2ef5fea5bae4a
Paul Jones <p.jones@outlook.com>
2024-12-21 23:50:46 +0000

    backend

--- terraform.tf
+++ terraform.tf
@@ -18,4 +18,11 @@ terraform {
       version = "~> 3.5"
     }
   }
+  backend "azurerm" {
+  resource_group_name  = "rg_Storage"
+  storage_account_name = "duddit2testingtfstate"
+  container_name       = "tfstate"
+  key                  = "tftestdiag.tfstate"
+  use_oidc             = true
+  }
 }
